f75eb926b617265029bbd73bf2c8b2c318d9e0e3
angesilwal12-byte <angesilwal12@gmail.com>
2026-09-25 13:54:29 +0545

    start

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input type="fisrtName" />
+      </div>
+   </form>
+     
+   
+  )
+}
+
+export default App
