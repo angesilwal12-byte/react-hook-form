e511e0fc61ccdbac5f9211e491a4993b9892a1ce
angesilwal12-byte <angesilwal12@gmail.com>
2026-09-25 15:15:17 +0545

    Readme added

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,41 @@ import './App.css'
 function App() {
 
   return (
-    <form>
-      <div>
-        <label htmlFor="firstName">First Name:</label>
-        <input type="fisrtName" />
-      </div>
-   </form>
-     
-   
-  )
+    <div className='main-container'>
+      <form>
+        <div>
+          <label htmlFor="jobTitle">Job Title</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="company">Company</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="location">Location</label>
+          <input type="location" />
+        </div>
+        <div>
+          <label htmlFor="companywebsite">Company Website</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="date">Join Date</label>
+          <input type="date" />
+        </div>
+        <div>
+          <label htmlFor="date">End Date</label>
+          <input type="date" />
+        </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <textarea name="descriptiom" id="description" rows={3}>
+            Description
+          </textarea>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App
